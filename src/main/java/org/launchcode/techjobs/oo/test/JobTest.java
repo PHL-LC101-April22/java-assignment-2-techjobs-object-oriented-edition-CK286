47d3e35cb7ada07b15441eef1a3e11b535dacf15
CK286 <CKing682b@gmail.com>
2022-09-29 20:57:49 -0400

    empty constructor test

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,22 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+  @Test
+    public  void  testSettingJobId() {
+      Job myJob = new Job();
+      Job yourJob = new Job();
+      assertNotEquals(myJob.getId(),yourJob.getId());
+  }
 
 }
