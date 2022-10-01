0ca50e41093967ef6d66475851ee0f6b8d648938
CK286 <CKing682b@gmail.com>
2022-10-01 14:49:28 -0400

    Created Base Class

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,4 +1,46 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class JobField {
+
+    //class variables
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+//constructors
+public JobField() {
+    id = nextId;
+    nextId++;
+}
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    //methods
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
 }
